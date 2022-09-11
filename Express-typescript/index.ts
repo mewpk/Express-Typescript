0fbc7cba2034b2ad5d4cebf6cbc7751b042690d7
MewPk <milzaza123@gmail.com>
2022-09-11 20:11:40 +0700

    Successful Example Node TS

--- Express-typescript/index.ts
+++ Express-typescript/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import express from 'express';
+
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
